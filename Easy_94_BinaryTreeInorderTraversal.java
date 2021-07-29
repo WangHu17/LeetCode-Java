d412cca968f41b6e639a46d61a3e0e03d2b6398b
wanghu <1936749601@qq.com>
2021-07-29 11:19:11 +0800

    94

---
+++ Easy_94_BinaryTreeInorderTraversal.java
@@ -0,0 +1,26 @@
+import bean.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author wanghu
+ * @discrption：
+ * @create 2021-07-29 10:34
+ */
+public class Easy_94_BinaryTreeInorderTraversal {
+
+    //我的方法
+    //Runtime: 0 ms, faster than 100.00% of Java online submissions for Binary Tree Inorder Traversal.
+    //Memory Usage: 37.3 MB, less than 49.04% of Java online submissions for Binary Tree Inorder Traversal.
+    List<Integer> list = new ArrayList<>();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if(root==null)return list;
+        inorderTraversal(root.left);
+        list.add(root.val);
+        inorderTraversal(root.right);
+        return list;
+    }
+
+}
